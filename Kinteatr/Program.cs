d376cbc63132bfb730dc36236ab61a6622a22d64
Yelyzaveta <elizabethch1524@gmail.com>
2025-11-04 10:34:09 +0200

    додані делегати

--- Kinteatr/Program.cs
+++ Kinteatr/Program.cs
@@ -1,16 +1,18 @@
-﻿using System;
+using System;
 using System.Text;
-
 
 namespace Kino
 {
+    public delegate void CinemaD();   
+    public delegate void SecurityD(); 
+    public delegate void LightD(); 
 
     class Cinema
     {
         private int seatsCount;
         private int viewersInside = 0;
 
-        public event Action NotPlaces;
+        public event CinemaD NotPlaces;
 
         public Cinema(int seats)
         {
@@ -20,15 +22,16 @@ namespace Kino
         public void PushViewer(Viewer viewer)
         {
             viewersInside++;
-            Console.WriteLine($"Глядач {viewer.Number} зайняв своє місце ({viewersInside}/{seatsCount})");
+            Console.WriteLine($"Глядач {viewer.Number} зайняв місце ({viewersInside}/{seatsCount})");
 
             if (viewersInside >= seatsCount)
             {
                 Console.WriteLine("Зал заповнено повністю!");
-                NotPlaces?.Invoke();
+                NotPlaces?.Invoke(); 
             }
         }
     }
+
     class Viewer
     {
         public int Number { get; }
@@ -41,7 +44,7 @@ namespace Kino
 
     class Security
     {
-        public event Action SwitchOff;
+        public event SecurityD SwitchOff;
 
         public void CloseZal()
         {
@@ -52,11 +55,11 @@ namespace Kino
 
     class Light
     {
-        public event Action Begin;
+        public event LightD Begin;
 
         public void Turn()
         {
-            Console.WriteLine("Вимикаємо світло!");
+            Console.WriteLine("Світло вимкнено!");
             Begin?.Invoke();
         }
     }
@@ -75,12 +78,11 @@ namespace Kino
             Console.WriteLine($"Починається фільм «{filmName}»!");
         }
     }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = Encoding.UTF8; 
+            Console.OutputEncoding = Encoding.UTF8;
 
             Console.Write("Введіть кількість місць у залі: ");
             int seats;
@@ -97,9 +99,9 @@ namespace Kino
             Light light = new Light();
             Hardware hardware = new Hardware(film);
 
-            cinema.NotPlaces += security.CloseZal;
-            security.SwitchOff += light.Turn;
-            light.Begin += hardware.FilmOn;
+            cinema.NotPlaces += security.CloseZal;  
+            security.SwitchOff += light.Turn;       
+            light.Begin += hardware.FilmOn;         
 
             for (int i = 1; i <= seats; i++)
             {
